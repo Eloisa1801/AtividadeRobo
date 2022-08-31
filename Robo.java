62df5027a3baf0178859df275a32b2cc0d9c65e0
Eloisa1801 <99847182+Eloisa1801@users.noreply.github.com>
2022-08-31 19:57:37 -0400

    Update Robo.java

--- Robo.java
+++ Robo.java
@@ -1,64 +1,40 @@
 
-import java.util.Scanner;
- 
 public class Robo {
- 
-    double x, y, novaPos, qtdMove;
-    public double getQtdMove() {
-        return qtdMove;
+    Coordenadas position;
+    char direction;
+    public Coordenadas getPosition() {
+        return position;
+    }
+    public void setPosition(Coordenadas position) {
+        this.position = position;
+    }
+    public char getDirection() {
+        return direction;
+    }
+    public void setDirection(char direction) {
+        this.direction = direction;
+    }
+    public Robo (double _x, double _y){
+        this.position = new Coordenadas(_x, _y);
+
     }
 
-    public void setQtdMove(double qtdMove) {
-        this.qtdMove = qtdMove;
+    public void move(char dir, int qtdMove){}
+
+    public void top(int pos){
+        
+
     }
 
-    public double getNovaPos() {
-        return novaPos;
+    public void down(int pos){
+        
     }
 
-    public void setNovaPos(double novaPos) {
-        this.novaPos = novaPos;
+    public void left(int pos){
+
     }
 
-    Scanner ler = new Scanner(System.in);
- 
-    public double getX() {
-        return x;
-    }
-
-    public void setX(Double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-    public void setY(Double y) {
-        this.y = y;
-    }
-
-    public Robo (double x, double y ){
-   
-        this.x = x;
-        this.y = y;
-    }
-   
- 
-    public  void top( ){
-   
-        this.novaPos = getY()+qtdMove;
- 
-    }
-
-    public void down( ){
-        this.novaPos = getY()-qtdMove;
-    }
-
-    public void left( ){
-        this.novaPos = getX()-qtdMove;
-    }
-
-    public void right( ){
-        this.novaPos = getX()+qtdMove;
+    public void right(int pos){
+        
     }
 }
