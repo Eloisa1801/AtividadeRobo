800af7fd66109311186aa6e7d41e00748c86e591
Eloisa1801 <99847182+Eloisa1801@users.noreply.github.com>
2022-08-31 21:11:20 -0400

    Update Robo.java

--- Robo.java
+++ Robo.java
@@ -1,40 +1,42 @@
 
 public class Robo {
-    Coordenadas position;
-    char direction;
+    Coordenadas position = new Coordenadas(2, 2);
+    // char direction;
+    public Robo(){}
+
     public Coordenadas getPosition() {
         return position;
     }
     public void setPosition(Coordenadas position) {
         this.position = position;
     }
-    public char getDirection() {
-        return direction;
-    }
-    public void setDirection(char direction) {
-        this.direction = direction;
-    }
+    // public char getDirection() {
+    //     return direction;
+    // }
+    // public void setDirection(char direction) {
+    //     this.direction = direction;
+    // }
     public Robo (double _x, double _y){
         this.position = new Coordenadas(_x, _y);
 
     }
 
-    public void move(char dir, int qtdMove){}
+    public void move(String dir, int qtdMove){}
 
-    public void top(int pos){
-        
-
-    }
-
-    public void down(int pos){
-        
-    }
-
-    public void left(int pos){
+    public void top( double qtdMove){
+        position.setY(position.getY()+qtdMove);
 
     }
 
-    public void right(int pos){
-        
+    public void down( int qtdMove){
+        position.setY(position.getY()-qtdMove);
+    }
+
+    public void left( int qtdMove){
+        position.setX(position.getX()+qtdMove);
+    }
+
+    public void right( int qtdMove){
+        position.setX(position.getX()-qtdMove);
     }
 }
